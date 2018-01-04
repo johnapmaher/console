bb612b0d843e9c3925e9a274eb05d53c25596af1
John Maher <johnapmaher@gmail.com>
2018-01-04 22:44:30 +0000

    fixes for messy tired coding

--- bin/console/ConsoleController.groovy
+++ bin/console/ConsoleController.groovy
@@ -4,6 +4,8 @@ import griffon.core.artifact.GriffonController
 import griffon.core.controller.ControllerAction
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
+
+import javax.inject.Inject
 import javax.annotation.Nonnull
 
 @ArtifactProviderFor(GriffonController)
@@ -11,7 +13,7 @@ class ConsoleController {
     @MVCMember @Nonnull
     ConsoleModel model
 
-    @inject
+    @Inject
     Evaluator evaluator
     
     @ControllerAction
@@ -22,7 +24,7 @@ class ConsoleController {
             result = evaluator.evaluate(model.scriptSource)
         } finally {
             model.enabled = true
-            model.scriptresults = result
+            model.scriptResult = result
         }
     }
 }
